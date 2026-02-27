af6c0b0ef8a27843a76aa086c892fa98ac74b1d3
omayrq <omayr.qureshi@gmail.com>
2026-02-27 20:36:54 +0500

    Serverles ETL CI/CD Pipeline

---
+++ scripts/etl_job.py
@@ -0,0 +1,6 @@
+import sys
+from awsglue.utils import getResolvedOptions
+
+print("🚀 ETL Job Starting...")
+# Your Spark/Python transformation logic goes here
+print("✅ ETL Job Completed Successfully.")
